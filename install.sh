9797880214aeeeb597a23f5410fbe65d06f27e14
Viktor Valentinov <vvalntnv@gmail.com>
2025-01-10 07:28:51 +0200

    mi staa

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-PLUGIN_DIR="~/.tmux/plugins/tpm"
+PLUGIN_DIR="$HOME/.tmux/plugins/tpm"
 
 if [ ! -d $PLUGIN_DIR ]; then
 	git clone https://github.com/tmux-plugins/tpm $PLUGIN_DIR
 fi
 
 ln -s $HOME/tmux-conf/.tmux.conf $HOME/.tmux.conf
+
+tmux source-file $HOME/.tmux.conf
