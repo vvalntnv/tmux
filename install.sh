14739650d977e97d1c7caf86f1f388f35558a3f6
vvalentinov <vvalentinov@neterra.net>
2025-01-10 06:45:07 +0200

    pochvame

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PLUGIN_DIR="~/.tmux/plugins/tpm"
+
+if [ ! -d $PLUGIN_DIR ]; then
+	git clone https://github.com/tmux-plugins/tpm $PLUGIN_DIR
+fi
+
+ln -s $HOME/tmux-conf/.tmux.conf $HOME/.tmux.conf
