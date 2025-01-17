60dc7f6847c4ceab0e62ea11d7f58c2117e946fb
vvalentinov <vvalentinov@neterra.net>
2025-01-17 14:08:25 +0200

    bate mnogo buckam (beshe ChatGPT)

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ if [ ! -d $PLUGIN_DIR ]; then
 	git clone https://github.com/tmux-plugins/tpm $PLUGIN_DIR
 fi
 
-ln -s $HOME/tmux-conf/.tmux.conf $HOME/.tmux.conf
+TMUX_DEST=$HOME/.tmux.conf
+
+if ! test -f $TMUX_DEST; then
+	ln -s $HOME/tmux-conf/.tmux.conf $HOME/.tmux.conf
+fi
+
 
 tmux source-file $HOME/.tmux.conf
